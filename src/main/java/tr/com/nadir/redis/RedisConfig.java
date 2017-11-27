5fb3644c2f712c1c86d6240d5f90bbbd3ec93e18
nadir.alkan <nadir.alkan@obss.com.tr>
2017-11-27 21:27:51 +0300

    Redis Config!

--- src/main/java/tr/com/nadir/redis/RedisConfig.java
+++ src/main/java/tr/com/nadir/redis/RedisConfig.java
@@ -1,5 +1,9 @@
 package tr.com.nadir.redis;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,24 +14,54 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import redis.clients.jedis.Protocol;
+
 @Configuration
 @EnableCaching
 @ComponentScan("tr.com.nadir.redis")
 public class RedisConfig {
+	@Value("${spring.redis.url}")
+	private String redisUrl;
+	// @Autowired
+	// private JedisConnectionFactory redisConnectionFactory;
+	// @Bean
+	// JedisConnectionFactory redisConnectionFactory() {
+	// JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+	// jedisConFactory.setHostName("ec2-34-252-202-201.eu-west-1.compute.amazonaws.com");
+	// jedisConFactory.setPassword("p670f00109627967a88be4cb3b0cd825003e851c5e77f85d72353743081cc3b5c");
+	// jedisConFactory.setPort(36159);
+	// return jedisConFactory;
+	// }
+
 	@Bean
-	JedisConnectionFactory redisConnectionFactory() {
-		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-		jedisConFactory.setHostName("ec2-34-252-202-201.eu-west-1.compute.amazonaws.com");
-		jedisConFactory.setPassword("p670f00109627967a88be4cb3b0cd825003e851c5e77f85d72353743081cc3b5c");
-		jedisConFactory.setPort(36159);
-		return jedisConFactory;
+	public JedisConnectionFactory redisConnectionFactory() {
+		try {
+			String redistogoUrl = System.getenv("REDISTOGO_URL");
+			if( redistogoUrl == null ){
+				redistogoUrl = redisUrl;
+			}
+			URI redistogoUri = new URI(redistogoUrl);
+			JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+			jedisConnFactory.setUsePool(true);
+			jedisConnFactory.setHostName(redistogoUri.getHost());
+			jedisConnFactory.setPort(redistogoUri.getPort());
+			jedisConnFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
+			jedisConnFactory.setPassword(redistogoUri.getUserInfo().split(":", 2)[1]);
+
+			return jedisConnFactory;
+
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-//
-//	@Bean
-//	public RedisSerializer<String> redisStringSerializer() {
-//		StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//		return stringRedisSerializer;
-//	}
+	//
+	// @Bean
+	// public RedisSerializer<String> redisStringSerializer() {
+	// StringRedisSerializer stringRedisSerializer = new
+	// StringRedisSerializer();
+	// return stringRedisSerializer;
+	// }
 
 	/**
 	 * public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory
@@ -41,7 +75,7 @@ public class RedisConfig {
 	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory cf) {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
 		redisTemplate.setConnectionFactory(cf);
-//		redisTemplate.setDefaultSerializer(redisSerializer);
+		// redisTemplate.setDefaultSerializer(redisSerializer);
 		return redisTemplate;
 	}
 
